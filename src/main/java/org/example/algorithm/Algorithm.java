c9a822a1ef028675970d337ed11af1177d7d25db
Adam Przywieczerski <adam.przywieczerski@gmail.com>
2024-06-07 17:27:21 +0200

    feat: return non-dominated solutions
    Only the best (non-dominated) solutions are returned by the algorithm

--- src/main/java/org/example/algorithm/Algorithm.java
+++ src/main/java/org/example/algorithm/Algorithm.java
@@ -5,6 +5,7 @@ import org.example.model.Solution;
 import org.example.util.SimulationData;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,7 +28,10 @@ public abstract class Algorithm {
         }
 
         Set<Solution> solutions = population.stream().map(Agent::getSolution).collect(Collectors.toSet());
-        return solutions;
+        System.out.println("\n\nSOLUTIONS: " + solutions.size());
+        Set<Solution> nonDominatedSolutions = getNonDominatedSolutions();
+        System.out.println("NONDOMINATED SOLUTIONS: " + nonDominatedSolutions.size());
+        return nonDominatedSolutions;
     }
 
     private boolean validState() {
@@ -36,6 +40,29 @@ public abstract class Algorithm {
 
     protected boolean checkStopCondition() {
         return iterations > simulationData.maxIterations;
+    }
+
+    protected Set<Solution> getNonDominatedSolutions() {
+        List<Solution> solutions = population.stream().map(Agent::getSolution).collect(Collectors.toList());
+        Set<Solution> nonDominatedSolutions = new HashSet<>();
+        for (int i = 0; i < solutions.size(); i++) {
+            Solution currSolution = solutions.get(i);
+            boolean dominated = false;
+            for (int j = 0; j < solutions.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                Solution otherSolution = solutions.get(j);
+                if (otherSolution.checkIfDominates(currSolution) == 1) {
+                    dominated = true;
+                    break;
+                }
+            }
+            if (!dominated) {
+                nonDominatedSolutions.add(currSolution);
+            }
+        }
+        return nonDominatedSolutions;
     }
 
     protected abstract void generateInitialPopulation();
