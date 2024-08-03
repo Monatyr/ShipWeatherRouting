55cc5bb86975233917fe8011051ca8b1b04cc2eb
Adam Przywieczerski <adam.przywieczerski@gmail.com>
2024-08-03 17:17:37 +0200

    feat: implemented a simple script to modify weather

--- src/main/java/org/example/physicalModel/PhysicalModel.java
+++ src/main/java/org/example/physicalModel/PhysicalModel.java
@@ -374,7 +374,7 @@ public abstract class PhysicalModel {
         double shapeCoefficient = getShapeCoefficient(windAngle);
         double maxWindSpeed = thresholdWindSpeed - shapeCoefficient * thresholdWindSpeedMargin;
         if ((maxWindSpeed - windSpeed) / maxWindSpeed <= 0) {
-            System.out.println(maxWindSpeed + " " + windSpeed);
+            System.out.println("Max wind speed (kn): " + maxWindSpeed + "\tCurrent wind speed (kn): " + windSpeed);
             System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         } else if ((maxWindSpeed - windSpeed) / maxWindSpeed > 1) {
             System.out.println("?????" + (maxWindSpeed - windSpeed) / maxWindSpeed);
